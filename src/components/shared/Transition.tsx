219072c97feefd51738077b565adeb2d3a1858a8
Wildan Zulfikar <hey@wzulfikar.com>
2022-08-07 19:36:47 +0300

    Remove fade transition in slideDown and slideUp

--- src/components/shared/Transition.tsx
+++ src/components/shared/Transition.tsx
@@ -7,41 +7,46 @@ const transitions = {
     fadeIn: {
       enter: tw`transition-opacity duration-75`,
       enterFrom: tw`opacity-0`,
-      enterTo: tw`opacity-100`
+      enterTo: tw`opacity-100`,
     },
     slideUp: {
       enter: tw`transform transition ease-in-out duration-500`,
-      enterFrom: tw`opacity-0 translate-y-24`,
-      enterTo: tw`opacity-100 translate-y-0`
-    }
+      enterFrom: tw`translate-y-24`,
+      enterTo: tw`translate-y-0`,
+    },
+    slideDown: {
+      enter: tw`transform transition ease-in-out duration-500`,
+      enterFrom: tw`-translate-y-24`,
+      enterTo: tw`translate-y-0`,
+    },
   },
   onLeave: {
     fadeOut: {
       leave: tw`transition-opacity duration-150`,
       leaveFrom: tw`opacity-100`,
-      leaveTo: tw`opacity-0`
+      leaveTo: tw`opacity-0`,
     },
     slideDown: {
       leave: tw`transform transition ease-in-out duration-500`,
-      leaveFrom: tw`opacity-100 translate-y-0`,
-      leaveTo: tw`opacity-0 translate-y-24`
+      leaveFrom: tw`translate-y-0`,
+      leaveTo: tw`translate-y-24`,
     },
     slideUp: {
       leave: tw`transform transition ease-in-out duration-500`,
-      leaveFrom: tw`opacity-100 translate-y-0`,
-      leaveTo: tw`opacity-0 -translate-y-24`
+      leaveFrom: tw`translate-y-0`,
+      leaveTo: tw`-translate-y-24`,
     },
     slideLeft: {
       leave: tw`transform transition ease-in-out duration-500`,
       leaveFrom: tw`opacity-100 translate-x-0`,
-      leaveTo: tw`opacity-0 -translate-x-24`
+      leaveTo: tw`opacity-0 -translate-x-24`,
     },
     slideRight: {
       leave: tw`transform transition ease-in-out duration-500`,
       leaveFrom: tw`opacity-100 translate-x-0`,
-      leaveTo: tw`opacity-0 translate-x-24`
-    }
-  }
+      leaveTo: tw`opacity-0 translate-x-24`,
+    },
+  },
 }
 
 type Props = {
