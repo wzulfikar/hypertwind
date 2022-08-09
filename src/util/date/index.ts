19e3f06078851bc48f6beb85b037d9e1c74f1e79
Wildan Zulfikar <hey@wzulfikar.com>
2022-08-09 14:59:45 +0300

    Fix import

--- src/util/date/index.ts
+++ src/util/date/index.ts
@@ -1,8 +1,13 @@
-import { addDays as _addDays, format } from "date-fns"
+import { addDays as _addDays, format, formatDistanceToNow } from "date-fns"
 
 export const formatDate = format
 
 export const addDays = (days: number, date: Date = new Date()) =>
   _addDays(date, days)
 
-export * from "./relativeTime"
+export const relativeTime = (datetime: string | Date, withSuffix = true) => {
+  const date = typeof datetime === "string" ? new Date(datetime) : datetime
+  return formatDistanceToNow(date, {
+    addSuffix: withSuffix,
+  })
+}
--- src/util/date/relativeTime.ts
+++
@@ -1,8 +0,0 @@
-import { formatDistanceToNow } from "date-fns"
-
-export const relativeTime = (datetime: string | Date, withSuffix = true) => {
-  const date = typeof datetime === "string" ? new Date(datetime) : datetime
-  return formatDistanceToNow(date, {
-    addSuffix: withSuffix,
-  })
-}
