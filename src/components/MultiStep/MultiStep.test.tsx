267279383ba42c8b3048bcf7ab175a1ed38a2610
Wildan Zulfikar <hey@wzulfikar.com>
2022-08-11 10:02:36 +0300

    Fix typing

--- src/components/MultiStep/MultiStep.test.tsx
+++ src/components/MultiStep/MultiStep.test.tsx
@@ -11,7 +11,11 @@ describe("MultiStep", () => {
     ],
   }
   it("renders without error", () => {
-    const { getByText } = render(<MultiStep steps={props.steps} />)
+    const { getByText } = render(
+      <MultiStep steps={props.steps}>
+        {({ idx }) => <div>Current step: {idx + 1}</div>}
+      </MultiStep>
+    )
 
     expect(getByText(props.steps[0].name)).toBeInTheDocument()
   })
