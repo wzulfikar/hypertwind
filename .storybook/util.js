a1789dd05f8256651e5ab9c8d2dd7e611604afd3
Wildan Zulfikar <hey@wzulfikar.com>
2022-08-16 17:32:51 +0300

    Move github repo to const

--- .storybook/util.js
+++ .storybook/util.js
@@ -1,7 +1,9 @@
+const GITHUB_REPO = 'https://github.com/wzulfikar/hypertwind';
+
 const seeInGithub = ({ component }) => {
   let path = ""
   if (component) path = `src/components/${component}/${component}.tsx`
-  return `See in Github: <a href='https://github.com/wzulfikar/hypertwind/tree/main/${path}'>${path}</a>`
+  return `See in Github: <a href='${GITHUB_REPO}/tree/main/${path}'>${path}</a>`
 }
 
 export const story = (component) => {
