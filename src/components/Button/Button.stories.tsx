9f3a61d4dcad40828adfe7bab8e898d193861d55
Wildan Zulfikar <hey@wzulfikar.com>
2022-08-17 22:13:20 +0300

    Use noop for handler in stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,20 +1,18 @@
-import { story } from "@storybook-util"
+import { story, noop } from "@storybook-util"
 import { tw } from "@components/util"
 import { Button } from "./Button"
 
 export default story(Button)
 
-const onClick = () => console.log("clicked!")
-
 export const ButtonSizes = () => (
   <div className={tw`flex flex-col space-y-2 w-32`}>
-    <Button onClick={onClick} size="sm">
+    <Button onClick={noop("onClick sm")} size="sm">
       Small (sm)
     </Button>
-    <Button onClick={onClick} size="md">
+    <Button onClick={noop("onClick md")} size="md">
       Medium (md)
     </Button>
-    <Button onClick={onClick} size="lg">
+    <Button onClick={noop("onClick lg")} size="lg">
       Large (lg)
     </Button>
   </div>
