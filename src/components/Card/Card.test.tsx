5653c6d5923bcbbbf60651cebd5480a5fc4e8606
Wildan Zulfikar <hey@wzulfikar.com>
2022-09-05 22:53:50 +0300

    f

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -5,9 +5,9 @@ describe("Card", () => {
   const props = {
     title: "Hello world!",
   }
+
   it("renders without error", () => {
     const { getByText } = render(<Card href="#" title={props.title} />)
-
     expect(getByText(props.title)).toBeInTheDocument()
   })
 })
