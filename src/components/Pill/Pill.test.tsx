16615100f077fc0ff0e18de50f10261da4bae17b
Wildan Zulfikar <hey@wzulfikar.com>
2022-09-05 22:18:07 +0300

    f

--- src/components/Pill/Pill.test.tsx
+++ src/components/Pill/Pill.test.tsx
@@ -5,6 +5,7 @@ describe("Pill", () => {
   const props = {
     children: "Hello world!",
   }
+
   it("renders without error", () => {
     const { getByText } = render(<Pill>{props.children}</Pill>)
 
