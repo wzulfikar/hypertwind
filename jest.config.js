46f4e73bee80a4dd73cb9c4520f4630e5bbecaf2
Wildan Zulfikar <wzulfikar@users.noreply.github.com>
2022-12-08 20:56:17 +0700

    Use pnpm (#10)
    * Use pnpm
    
    * f
    
    * Use steps from https://github.com/pnpm/action-setup/issues/8
    
    * Follow guide from https://github.com/pnpm/action-setup
    
    * pnpm install --silent --frozen-lockfile
    
    * f

--- jest.config.js
+++ jest.config.js
@@ -15,7 +15,7 @@ module.exports = {
       { jsc: { transform: { react: { runtime: "automatic" } } } },
     ],
   },
-  transformIgnorePatterns: ["!node_modules/@twind/core/core.browser.js", "!node_modules/@tiptap/core/dist/tiptap-core.cjs"],
+  transformIgnorePatterns: ["!node_modules/@twind/core/core.browser.js"],
   testPathIgnorePatterns: ["<rootDir>/node_modules/"],
   moduleNameMapper: {
     ...pathsToModuleNameMapper(paths, { prefix: "<rootDir>/" }),
